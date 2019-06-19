2f0b5527e789f5f99883b46485190a81168cce17
charlie-ccw <2392687133@qq.com>
2019-06-19 16:12:50 +0100

    Update cs coding.py

--- cs coding.py
+++ cs coding.py
@@ -1,1 +1,13 @@
-import
+import pygame
+pygame.init()
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED   = (255,0,0)
+BLUE  = (0,0,255)
+
+size = (700,500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Charlie's 'bullet hell'")
+
