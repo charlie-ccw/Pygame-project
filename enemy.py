6bcad56e747999fcdfe8e70fc2a53247b292960a
charlie-ccw <2392687133@qq.com>
2019-07-12 16:26:38 +0800

    define the enemt plane

---
+++ enemy.py
@@ -0,0 +1,89 @@
+import pygame
+from random import *
+
+ # here we difine the smallest enemy plane
+class Enemy1(pygame.sprite.Sprite):
+    def __init__(self,size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/enemy1.jpg").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.width = 640
+        self.height = 960
+        self.speed = 4
+        self.rect.left = randint(0,self.width - self.rect.width)
+        self.rect.bottom = randint(self.height*-5,0)
+
+
+     # here we difine the movemont of the enemy
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top +=self.speed
+        else:
+            reset()
+
+     # here we define how the enemy reset in the game
+    def rest(self):
+        self.rect.left = randint(0,self.width - self.rect.width)
+        self.rect.bottom = randint(self.height*-5,0)
+        
+
+
+
+
+
+ # here we difine the middle size enemy plane
+class Enemy2(pygame.sprite.Sprite):
+    def __init__(self,size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/enemy2.jpg").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.width = 640
+        self.height = 960
+        self.speed = 2
+        self.rect.left = randint(0,self.width - self.rect.width)
+        self.rect.bottom = randint(self.height*-10,-self.height)
+
+
+     # here we difine the movemont of the enemy
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top +=self.speed
+        else:
+            reset()
+
+     # here we define how the enemy reset in the game
+    def rest(self):
+        self.rect.left = randint(0,self.width - self.rect.width)
+        self.rect.bottom = randint(self.height*-10,0)
+
+
+
+
+
+ # here we difine the biggest size enemy plane
+class Enemy3(pygame.sprite.Sprite):
+    def __init__(self,size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/enemy3.jpg").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.width = 640
+        self.height = 960
+        self.speed = 1
+        self.rect.left = randint(0,self.width - self.rect.width)
+        self.rect.bottom = randint(self.height*-12,-self.height*2)
+
+
+     # here we difine the movemont of the enemy
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top +=self.speed
+        else:
+            reset()
+
+     # here we define how the enemy reset in the game
+    def rest(self):
+        self.rect.left = randint(0,self.width - self.rect.width)
+        self.rect.bottom = randint(self.height*-10,0)
