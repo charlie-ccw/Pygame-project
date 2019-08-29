900ab57c62884493dbbd0ebeec29c982a99d9399
charlie-ccw <2392687133@qq.com>
2019-08-29 11:40:31 +0800

    change the colur of player's bullet

--- bullet.py
+++ bullet.py
@@ -4,7 +4,7 @@ import pygame
 class Bullet1(pygame.sprite.Sprite):
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("images/18.png").convert_alpha()
+        self.image = pygame.image.load("images/19.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = position
         self.speed = 12
