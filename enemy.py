a9171c799d6e637b5f595c949953ec36da05fd37
charlie-ccw <2392687133@qq.com>
2019-09-03 22:41:05 +0800

    Update enemy.py
    add the enemy which is from bottom of the screen

--- enemy.py
+++ enemy.py
@@ -250,3 +250,43 @@ class Enemy6(pygame.sprite.Sprite):
         self.active = True
         self.rect.left = randint(700,1000)
         self.rect.bottom = randint(-100,150)
+
+
+ # here we difine the smallest enemy plane
+class Enemy7(pygame.sprite.Sprite):
+    def __init__(self,size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("images/6.png").convert_alpha()
+        # here we difine the destory picture of the enemy
+        self.destroy_images = []
+        self.destroy_images.extend([\
+             pygame.image.load("images/7.png").convert_alpha(),\
+             pygame.image.load("images/8.png").convert_alpha(),\
+             pygame.image.load("images/9.png").convert_alpha(),\
+             pygame.image.load("images/1.png").convert_alpha(),\
+             ])
+        self.rect = self.image.get_rect()
+        self.width = 700
+        self.height = 960
+        self.speed = 4
+        # here we set the condition of the picture
+        self.active = True
+        self.rect.left = randint(100,600)
+        self.rect.bottom = randint(1000,2000)
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+     # here we difine the movemont of the enemy
+    def move(self):
+        if self.rect.top > 0:
+            self.rect.top -= 3
+            self.rect.left += 3*math.sin(self.rect.top/40)
+        else:
+            self.reset()
+
+     # here we define how the enemy reset in the game
+    def reset(self):
+        self.active = True
+        self.rect.left = randint(100,600)
+        self.rect.bottom = randint(1000,2000)
